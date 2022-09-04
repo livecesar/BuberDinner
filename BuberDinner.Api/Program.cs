238607c5959815cc2bc3baa6282b7cb245f93996
LiveCesar <livecesar@gmail.com>
2022-09-04 12:54:16 -0500

    Error handling through FilterAttribute, added to controllers in the program configuration

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,4 +1,5 @@
-using BuberDinner.Api.Midddleware;
+using BuberDinner.Api.Filter;
+//using BuberDinner.Api.Midddleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 
@@ -8,12 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
     
-    builder.Services.AddControllers();
+    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
 {
-    app.UseMiddleware<ErrorHandlingMiddleware>();
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers();
 
