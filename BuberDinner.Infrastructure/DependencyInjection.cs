3f4b769c0a698498cb6f7e1ac6afc3a66d22f170
LiveCesar <livecesar@gmail.com>
2022-09-04 06:53:07 -0500

    Jwt authentication implemented for regitration

--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,16 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Services;
+using BuberDinner.Infrastructure.Authentication;
+using BuberDinner.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Infrastructure;
 
 public static class DependencyInjection{
     public static IServiceCollection AddInfrastructure(this IServiceCollection services){
-            return services;
+
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        return services;
     }
 }
