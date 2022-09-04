30ff03e5aeed53280268ba249aee1b59d6dbf33a
LiveCesar <livecesar@gmail.com>
2022-09-04 13:13:05 -0500

    ProblemDetails object implemented for the ErrorHandlingFilterAttribute

--- BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
+++ BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -9,10 +10,13 @@ public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
     {
         var exception = context.Exception;
 
-        context.Result = new ObjectResult(new{error = "An error occurred while processing your request."})
+        var problemDetails = new ProblemDetails
         {
-            StatusCode = 500
+            Title = "An error occurred while processing your request.",
+            Status = (int)HttpStatusCode.InternalServerError,
         };
+
+        context.Result = new ObjectResult(problemDetails);
         context.ExceptionHandled = true;
     }
 
