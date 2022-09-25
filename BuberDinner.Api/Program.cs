2dc9dd6e420a9caafe8eb4a96d7beb3de06d22af
LiveCesar <livecesar@gmail.com>
2022-09-25 16:00:36 -0500

    Applies global error handling throuht Problem details/middleware error handling

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,7 +1,9 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Api.Filter;
 //using BuberDinner.Api.Midddleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -9,12 +11,16 @@ var builder = WebApplication.CreateBuilder(args);
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
     
-    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+    //builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+    builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
     //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
 
